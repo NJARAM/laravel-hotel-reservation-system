6d4563a21ae13d51a6145b63fdf20020475db0ce
Moses Njara <njaramoses@gmail.com>
2019-04-29 13:18:29 +0300

    minor updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
     <div class="row column">
       <hr>
       <ul class="menu">
-        <li class="float-right">Copyright 2018</li>
+        <li class="float-right">Copyright 2019</li>
       </ul>
     </div>
 
